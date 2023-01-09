6be67f7defdf1e03a438a5761031b1987c035c30
Abdul Fatao Abdulrahman <abdulfataoabdulrahman@gmail.com>
2023-01-09 10:03:20 +0000

    Create Password.cpp

---
+++ Password.cpp
@@ -0,0 +1,31 @@
+include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int password;
+
+cout <<<"Enter a password to bruteforce.. : ";
+
+cin >> password;
+
+for (int i = 0; i < 9999999999; i++) {
+
+ if (i = password) {
+
+cout << "The brute-forced password is = <<< i <<< endl;
+
+break;
+
+}
+
+cout << "Loading.. [" << i << "]\n";
+
+}
+
+return 0;
+
+ }
